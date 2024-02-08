531da73c634258f5ae87f048c635e9b83e6b7ee5
Pratya Palee <pratya.palee@gmail.com>
2024-02-09 01:20:50 +0700

    usertype/models.py update

--- lineforinterns/lineforinterns/settings.py
+++ lineforinterns/lineforinterns/settings.py
@@ -20,78 +20,79 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6go#cd2g_($wkki8$3@l=)gw@yc98*a@-^gy3di7jzr31fm2u'
+SECRET_KEY = "django-insecure-e6go#cd2g_($wkki8$3@l=)gw@yc98*a@-^gy3di7jzr31fm2u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'usertype',
-    'student',
-    'company',
-    'multiple_auth',
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.facebook',
-    'allauth.socialaccount.providers.line',
-    'linebot',
-
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "usertype",
+    "student",
+    "company",
+    "multiple_auth",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.facebook",
+    "allauth.socialaccount.providers.line",
+    "linebot",
 ]
+
+SITE_ID = 1
+AUTH_USER_MODEL = "usertype.CustomUser"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
-ROOT_URLCONF = 'lineforinterns.urls'
+ROOT_URLCONF = "lineforinterns.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'lineforinterns.wsgi.application'
+WSGI_APPLICATION = "lineforinterns.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -101,35 +102,33 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'multiple_auth.backends.MultipleAuthentication',
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "multiple_auth.backends.MultipleAuthentication",
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 )
 
-MULTIPLE_AUTH = {
-    'auth_fields': ['username', 'email']
-}
+MULTIPLE_AUTH = {"auth_fields": ["username", "email"]}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -139,9 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- lineforinterns/usertype/models.py
+++ lineforinterns/usertype/models.py
@@ -1,7 +1,107 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.contrib.auth.models import BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class CustomUser(AbstractUser):
-    user_type_data = ((1, "Student"), (2, "company"))
-    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
-    email = models.EmailField(unique=True)
+    class Role(models.TextChoices):
+        STUDENT = "STUDENT", "Student"
+        COMPANY = "COMPANY", "Company"
+        ADMIN = "ADMIN", "Admin"
+
+    base_role = Role.ADMIN
+
+    role = models.CharField(max_length=50, choices=Role.choices, default=base_role)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+        return super().save(*args, **kwargs)
+
+
+# Student-----------------------------------------------------------------
+class StudentManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=CustomUser.Role.STUDENT)
+
+
+class Student(CustomUser):
+
+    base_role = CustomUser.Role.STUDENT
+
+    student = StudentManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "Only for Student"
+
+
+@receiver(post_save, sender=Student)
+def create_student_profile(sender, instance, created, **kwargs):
+    if created and instance.role == CustomUser.Role.STUDENT:
+        StudentProfile.objects.create(user=instance)
+
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    student_id = models.IntegerField(unique=True, primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    nick_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    date_of_birth = models.DateField
+
+
+# Student----------------------------------------------------------------
+
+
+# Company----------------------------------------------------------------
+
+
+class CompanyManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs):
+        results = super().get_queryset(*args, **kwargs)
+        return results.filter(role=CustomUser.Role.COMPANY)
+
+
+class Company(CustomUser):
+
+    base_role = CustomUser.Role.COMPANY
+
+    company = CompanyManager()
+
+    class Meta:
+        proxy = True
+
+    def welcome(self):
+        return "Only for Company"
+
+
+@receiver(post_save, sender=Company)
+def create_company_profile(sender, instance, created, **kwargs):
+    if created and instance.role == CustomUser.Role.COMPANY:
+        CompanyProfile.objects.create(user=instance)
+
+
+class CompanyProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    company_name_eng = models.CharField(max_length=100, unique=True, primary_key=True)
+    company_name_thai = models.CharField(max_length=100)
+    company_email = models.EmailField()
+    company_phone = models.CharField(max_length=15)
+    company_address = models.TextField()
+    company_website = models.URLField()
+    company_logo = models.ImageField(upload_to="company_logo/")
+    company_description = models.TextField()
+    company_established = models.DateField
+
+
+# Company----------------------------------------------------------------
